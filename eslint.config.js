00637997a9a6a00eefe390baca946cfdcd19e134
culi <culitif@tuta.io>
2025-11-19 21:18:49 -0800

    chore: fix react hooks linter warnings
    Turns out there's no such eslint rule as `printWidth` (that's a prettier thing). It also turns out that attempting to add an eslint rule that doesn't exist will crash the entire linter :( I finally figured this out after messing with and refactoring my eslint config file for awhile and even updating some dependencies.

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,32 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import reactHooksPlugin from 'eslint-plugin-react-hooks'
+import reactRefreshPlugin from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default tseslint.config(
-  { ignores: ["dist"] },
+export default defineConfig([
+  globalIgnores(['dist']),
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooksPlugin.configs.flat.recommended,
+      reactRefreshPlugin.configs.vite,
+    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
     rules: {
-      ...reactHooks.configs.recommended.rules,
       "@typescript-eslint/no-unused-vars": [
         "error",
         { argsIgnorePattern: "^_", caughtErrorsIgnorePattern: "^_", varsIgnorePattern: "^_" },
       ],
-      printWidth: "warn",
+      "max-len": ["warn", { code: 100 }],
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "react-hooks/exhaustive-deps": "error",
     },
   }
-);
+])
